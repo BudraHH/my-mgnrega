51cdd002c159c45138a983a9413072a7fdc6422b
Hari Hara Budra <hariharabudra@gmail.com>
2025-11-01 23:29:05 +0530

    db config

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,11 +2,13 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    user: process.env.DB_USER || 'postgres',
-    password: '123',
-    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER ,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST ,
     port: process.env.DB_PORT || 5432,
-    database: process.env.DB_NAME || 'my_mgnrega',
+    database: process.env.DB_NAME ,
+    connectionString: process.env.DB_PORTL,
+    ssl: { rejectUnauthorized: false },
 });
 
 pool.on('error', (err) => {
